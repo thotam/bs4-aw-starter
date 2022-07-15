2dbc9a6ed4cd7e0dff36ab9c5d649e968cd2a8fc
thotam <thanhtamtqno1@gmail.com>
2022-07-15 11:42:38 +0700

    Update HelperTrait.php

--- src/Traits/HelperTrait.php
+++ src/Traits/HelperTrait.php
@@ -6,152 +6,183 @@ use Illuminate\Support\Str;
 
 trait HelperTrait
 {
-	/**
-	 * Install the middleware to a group in the application Http Kernel.
-	 *
-	 * @param  string  $after
-	 * @param  string  $name
-	 * @param  string  $group
-	 * @return void
-	 */
-	protected function installMiddlewareAfter($after, $name, $group = 'web')
-	{
-		$httpKernel = file_get_contents(app_path('Http/Kernel.php'));
+    /**
+     * Install the middleware to a group in the application Http Kernel.
+     *
+     * @param  string  $after
+     * @param  string  $name
+     * @param  string  $group
+     * @return void
+     */
+    protected function installMiddlewareAfter($after, $name, $group = 'web')
+    {
+        $httpKernel = file_get_contents(app_path('Http/Kernel.php'));
 
-		$middlewareGroups = Str::before(Str::after($httpKernel, '$middlewareGroups = ['), '];');
-		$middlewareGroup = Str::before(Str::after($middlewareGroups, "'$group' => ["), '],');
+        $middlewareGroups = Str::before(Str::after($httpKernel, '$middlewareGroups = ['), '];');
+        $middlewareGroup = Str::before(Str::after($middlewareGroups, "'$group' => ["), '],');
 
-		if (!Str::contains($middlewareGroup, $name)) {
-			$modifiedMiddlewareGroup = str_replace(
-				$after . ',',
-				$after . ',' . PHP_EOL . '            ' . $name . ',',
-				$middlewareGroup,
-			);
+        if (!Str::contains($middlewareGroup, $name)) {
+            $modifiedMiddlewareGroup = str_replace(
+                $after . ',',
+                $after . ',' . PHP_EOL . '            ' . $name . ',',
+                $middlewareGroup,
+            );
 
-			file_put_contents(app_path('Http/Kernel.php'), str_replace(
-				$middlewareGroups,
-				str_replace($middlewareGroup, $modifiedMiddlewareGroup, $middlewareGroups),
-				$httpKernel
-			));
-		}
-	}
+            file_put_contents(app_path('Http/Kernel.php'), str_replace(
+                $middlewareGroups,
+                str_replace($middlewareGroup, $modifiedMiddlewareGroup, $middlewareGroups),
+                $httpKernel
+            ));
+        }
+    }
 
-	/**
-	 * Update Config file.
-	 *
-	 * @param  string  $search
-	 * @param  string  $replace
-	 * @param  string  $path
-	 * @return void
-	 */
-	protected function updateConfig($search, $replace, $path)
-	{
+    /**
+     * Update Config file.
+     *
+     * @param  string  $search
+     * @param  string  $replace
+     * @param  string  $path
+     * @return void
+     */
+    protected function updateConfig($search, $replace, $path)
+    {
 
-		$this->replaceInFile($search, $replace, $path);
-	}
+        $this->replaceInFile($search, $replace, $path);
+    }
 
-	/**
-	 * Install the route middleware to $routeMiddleware Http Kernel.
-	 *
-	 * @param  string  $after
-	 * @param  string  $name
-	 * @return void
-	 */
-	protected function installRouteMiddlewareAfter($after, $name)
-	{
-		$httpKernel = file_get_contents(app_path('Http/Kernel.php'));
+    /**
+     * Install the route middleware to $routeMiddleware Http Kernel.
+     *
+     * @param  string  $after
+     * @param  string  $name
+     * @return void
+     */
+    protected function installRouteMiddlewareAfter($after, $name)
+    {
+        $httpKernel = file_get_contents(app_path('Http/Kernel.php'));
 
-		$routeMiddleware = Str::before(Str::after($httpKernel, '$routeMiddleware = ['), '];');
+        $routeMiddleware = Str::before(Str::after($httpKernel, '$routeMiddleware = ['), '];');
 
-		if (!Str::contains($routeMiddleware, $name)) {
-			$modifiedRouteMiddleware = str_replace(
-				$after . ',',
-				$after . ',' . PHP_EOL . '        ' . $name . ',',
-				$routeMiddleware,
-			);
+        if (!Str::contains($routeMiddleware, $name)) {
+            $modifiedRouteMiddleware = str_replace(
+                $after . ',',
+                $after . ',' . PHP_EOL . '        ' . $name . ',',
+                $routeMiddleware,
+            );
 
-			file_put_contents(app_path('Http/Kernel.php'), str_replace(
-				$routeMiddleware,
-				$modifiedRouteMiddleware,
-				$httpKernel
-			));
-		}
-	}
+            file_put_contents(app_path('Http/Kernel.php'), str_replace(
+                $routeMiddleware,
+                $modifiedRouteMiddleware,
+                $httpKernel
+            ));
+        }
+    }
 
-	/**
-	 * Add github vcs to composer.
-	 *
-	 * @param  string  $name
-	 * @return void
-	 */
-	protected function addRepositories($name)
-	{
-		$composers = json_decode(file_get_contents(base_path('composer.json')), true);
-		if (array_key_exists("repositories", $composers)) {
-			$repositories = $composers['repositories'];
-		} else {
-			$repositories = [];
-		}
+    /**
+     * Add github vcs to composer.
+     *
+     * @param  string  $name
+     * @return void
+     */
+    protected function addRepositories($name)
+    {
+        $composers = json_decode(file_get_contents(base_path('composer.json')), true);
+        if (array_key_exists("repositories", $composers)) {
+            $repositories = $composers['repositories'];
+        } else {
+            $repositories = [];
+        }
 
-		$collect_repositories = collect($repositories)->filter(function ($value, $key) use ($name) {
-			return Str::contains($value['url'], $name);
-		});
+        $collect_repositories = collect($repositories)->filter(function ($value, $key) use ($name) {
+            return Str::contains($value['url'], $name);
+        });
 
-		if ($collect_repositories->count() == 0) {
-			$repositories[] = [
-				'type' => 'vcs',
-				'url' => "git@github.com:" . $name . ".git"
-			];
-		}
+        if ($collect_repositories->count() == 0) {
+            $repositories[] = [
+                'type' => 'vcs',
+                'url' => "git@github.com:" . $name . ".git"
+            ];
+        }
 
-		$composers['repositories'] = $repositories;
+        $composers['repositories'] = $repositories;
 
-		file_put_contents(
-			base_path('composer.json'),
-			json_encode($composers, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . PHP_EOL
-		);
-	}
+        file_put_contents(
+            base_path('composer.json'),
+            json_encode($composers, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . PHP_EOL
+        );
+    }
 
-	/**
-	 * Insert After string.
-	 *
-	 * @param  string  $after
-	 * @param  string  $insert
-	 * @param  string  $path
-	 * @param  string  $space
-	 * @return void
-	 */
-	protected function insertAfter($after, $insert, $path, $space = "	")
-	{
-		$contents = file_get_contents($path);
+    /**
+     * Insert After string.
+     *
+     * @param  string  $after
+     * @param  string  $insert
+     * @param  string  $path
+     * @param  string  $space
+     * @return void
+     */
+    protected function insertAfter($after, $insert, $path, $space = "	")
+    {
+        $contents = file_get_contents($path);
 
-		if (!Str::contains($contents, $insert)) {
-			file_put_contents($path, str_replace(
-				$after,
-				$after . PHP_EOL . $space . $insert,
-				$contents,
-			));
-		}
-	}
+        if (!Str::contains($contents, $insert)) {
+            file_put_contents($path, str_replace(
+                $after,
+                $after . PHP_EOL . $space . $insert,
+                $contents,
+            ));
+        }
+    }
 
-	/**
-	 * ImplementsContracts
-	 *
-	 * @param  mixed $find
-	 * @param  mixed $replace
-	 * @param  mixed $path
-	 * @return void
-	 */
-	protected function ImplementsContracts($find, $replace, $path)
-	{
-		$contents = file_get_contents($path);
+    /**
+     * ImplementsContracts
+     *
+     * @param  mixed $find
+     * @param  mixed $replace
+     * @param  mixed $path
+     * @return void
+     */
+    protected function ImplementsContracts($find, $replace, $path)
+    {
+        $contents = file_get_contents($path);
 
-		if (!Str::contains($contents, $replace)) {
-			file_put_contents($path, str_replace(
-				$find,
-				$replace,
-				$contents,
-			));
-		}
-	}
+        if (!Str::contains($contents, $replace)) {
+            file_put_contents($path, str_replace(
+                $find,
+                $replace,
+                $contents,
+            ));
+        }
+    }
+
+    /**
+     * addPropertyArray
+     *
+     * @param  string $name
+     * @param  string $after
+     * @param  string $insert
+     * @param  string $path
+     * @param  string $space
+     * @return void
+     */
+    protected function addPropertyArray($name, $after, $insert, $path, $space = '        ')
+    {
+        $contents = file_get_contents($path);
+
+        $sub_contents = Str::before(Str::after($contents, $name . ' = ['), '];');
+
+        if (!Str::contains($sub_contents, $insert)) {
+            $replace_sub_contents = str_replace(
+                $after,
+                $after . PHP_EOL . $space . $insert,
+                $sub_contents,
+            );
+
+            file_put_contents($path, str_replace(
+                $sub_contents,
+                $replace_sub_contents,
+                $contents
+            ));
+        }
+    }
 }
